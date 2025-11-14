091dca5cf61c8203de4dae80165f9eaf3ec44a16
bochamaakram <akrambouchama0@gmail.com>
2025-11-14 21:55:14 +0100

    gadina notification

--- ajouter.js
+++ ajouter.js
@@ -185,3 +185,11 @@ function toggleMobileMenu() {
     alert("Menu mobile - Fonctionnalité à développer selon les besoins spécifiques");
   }
 }
+
+
+
+function launch_toast() {
+    var x = document.getElementById("toast")
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+}
